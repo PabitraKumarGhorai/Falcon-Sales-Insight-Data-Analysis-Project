b8a686d61e47e9413d584d9fbef9bc0bd3df71a4
Pabitra Kumar Ghorai <50179771+PabitraKumarGhorai@users.noreply.github.com>
2023-05-20 13:35:04 +0530

    Add files via upload
    Final code deployment

---
+++ Project_Falcon_Sales Databricks Files/Master_Notebook.py
@@ -0,0 +1,23 @@
+# Databricks notebook source
+# DBTITLE 1,FALCON_STG_CUSTOMER
+# MAGIC  %run /Users/ghoraipabitrakumar@gmail.com/Project_Falcon_Sales/FALCON_STG_CUSTOMER
+
+# COMMAND ----------
+
+# DBTITLE 1,FALCON_STG_DATE
+# MAGIC  %run /Users/ghoraipabitrakumar@gmail.com/Project_Falcon_Sales/FALCON_STG_DATE
+
+# COMMAND ----------
+
+# DBTITLE 1,FALCON_STG_MARKET
+# MAGIC  %run /Users/ghoraipabitrakumar@gmail.com/Project_Falcon_Sales/FALCON_STG_MARKET
+
+# COMMAND ----------
+
+# DBTITLE 1,FALCON_STG_PRODUCTS
+# MAGIC  %run /Users/ghoraipabitrakumar@gmail.com/Project_Falcon_Sales/FALCON_STG_PRODUCTS
+
+# COMMAND ----------
+
+# DBTITLE 1,FALCON_STG_TRANSACTION
+# MAGIC  %run /Users/ghoraipabitrakumar@gmail.com/Project_Falcon_Sales/FALCON_STG_TRANSACTION
